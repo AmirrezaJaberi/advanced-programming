a39320442a508c8f1618b9c9d32cd6990642c028
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-07 22:15:06 +0330

    Adde for-loop structure file

---
+++ Introduction/for-loop.py
@@ -0,0 +1,11 @@
+# Loop with start, stop, and step values
+for i in range(start, stop, step):
+    # do something
+
+# Loop with start and stop values (default step is 1)
+for i in range(start, stop):
+    # do something
+
+# Loop with stop value (default start is 0 and step is 1)
+for i in range(stop):
+    # do something
