844272adf383045df2bbf36267948404302df02a
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-07 21:56:44 +0330

    Added condition file

---
+++ Introduction/condition.py
@@ -0,0 +1,11 @@
+# Conditional structure in Python
+
+# If statement: checks if the condition is true (1 evaluates to True)
+if 1:
+    print("Here is 1")
+# Elif statement: checks if the previous condition was false and this condition is true (0 evaluates to False)
+elif 0:
+    print("Here is 2")
+# Else statement: executes if all previous conditions are false
+else:
+    print("Here is 3")
--- Introduction/practice-2.py
+++ Introduction/practice-2.py
@@ -2,10 +2,10 @@
 # Write a program that takes an input number and determines if it is even or odd.
 
 # Input: Get a number from user input
-number = int(input('Enter a number: '))
+number = int(input("Enter a number: "))
 
 # Check if the number is even or odd
 if number % 2 == 0:
-    print('The number', number, 'is even.')
+    print("The number", number, "is even.")
 else:
-    print('The number', number, 'is odd.')
+    print("The number", number, "is odd.")
