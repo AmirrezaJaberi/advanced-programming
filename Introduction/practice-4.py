d7283644de32c0c3703b64616196303383d0f166
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-07 22:27:58 +0330

    Added practice 4 file (calculate the sum of numbers from 1 to 10)

---
+++ Introduction/practice-4.py
@@ -0,0 +1,11 @@
+## Practice 4 :
+# Program to calculate the sum of numbers from 1 to 10
+# Initialize sum to zero
+total = 0
+
+# Loop through numbers 1 to 10 and add them to total
+for num in range(1, 11):
+    total += num
+    # OR total = total + num
+# Print the result
+print("Sum of numbers from 1 to 10 is:", total)
