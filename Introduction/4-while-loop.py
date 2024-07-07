6ba92a3e6524b1a38cea5b2d160ed7dbdbdcde2b
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-07 23:00:10 +0330

    Added to example for while loop

--- Introduction/4-while-loop.py
+++ Introduction/4-while-loop.py
@@ -1,3 +1,11 @@
 # Loop
 while condition:
     # do something
+
+# Loop
+while True:
+    print('infinity')
+
+# This while loop will never execute because the condition is False
+while False:
+    print('Not run')
