e59f2497cbb785ba0fa6d82287e7e796127b3946
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-07 21:11:57 +0330

    Added input file with an example

---
+++ Introduction/input.py
@@ -0,0 +1,6 @@
+# Input: Get an entry via command line
+# Arguments: (text) [optional]
+entry = input('Enter a number: ')  # Prompt the user to enter a number
+
+# Print the input value
+print(entry)  # Display the entered value in the console
