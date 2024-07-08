2041c5e42b0a8a0fe16cbfb8a754d61ec9f1b6a1
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-08 18:41:01 +0330

    Added int.py file to data structure

---
+++ Data Structures/int.py
@@ -0,0 +1,7 @@
+# int: Convert a string or float to an integer
+num_str = "10"
+num_float = 10.5
+int_num_str = int(num_str)
+int_num_float = int(num_float)
+print(int_num_str)  # Output: 10
+print(int_num_float)  # Output: 10
