bbb65ffe7ffb0a61a47aff52b5155f047af6556b
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-08 18:42:43 +0330

    Added range.py file to data structure

---
+++ Data Structures/ranges.py
@@ -0,0 +1,18 @@
+# Example of converting range to a list
+numbers_list = list(range(1, 10, 2))
+print(numbers_list)
+
+# range with start, stop, and step
+for i in range(1, 10, 2):
+    print(i)
+# Output: 1, 3, 5, 7, 9
+
+# range with start and stop
+for i in range(2, 7):
+    print(i)
+# Output: 2, 3, 4, 5, 6
+
+# range with stop
+for i in range(5):
+    print(i)
+# Output: 0, 1, 2, 3, 4
