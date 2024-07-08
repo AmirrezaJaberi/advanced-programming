b1b4f9de9fc531601531e90c1db38cebbc6f673c
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-08 18:41:25 +0330

    Added float.py file to data structure

---
+++ Data Structures/float.py
@@ -0,0 +1,7 @@
+# float: Convert a string or integer to a float
+num_str = "10.5"
+num_int = 10
+float_num_str = float(num_str)
+float_num_int = float(num_int)
+print(float_num_str)  # Output: 10.5
+print(float_num_int)  # Output: 10.0
