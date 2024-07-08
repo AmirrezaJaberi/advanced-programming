e8340c5e5c9e3f681b0289b52a651e3455b672da
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-07-08 18:41:46 +0330

    Added bool.py file to data structure

---
+++ Data Structures/bool.py
@@ -0,0 +1,4 @@
+# bool: Convert different types to a boolean
+user_input = input("Enter something: ")
+bool_input = bool(user_input)
+print(bool_input)  # Output: True if the user enters anything non-empty, False otherwise
